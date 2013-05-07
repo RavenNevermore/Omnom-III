c0eb9b07312b679f9a2209e97b3e25fbf61c971e
RabenNevermore <TheRavenNevermore@web.de>
2013-05-07 12:56:07 +0200

    NUnit unittest setup

---
+++ Omnom-III-Game/omnom-nunit-tests/SanityTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace omnom_nunit_tests
+{
+    [TestFixture]
+    class SanityTest
+    {
+        [Test]
+        public void testSantiy() {
+            Assert.AreEqual(4, 2+2);
+        }
+    }
+}
